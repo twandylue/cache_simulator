ec7241aad04d149d2b5576a783235f9023ddcf38
AndyLu <laserlue@gmail.com>
2025-03-07 14:22:44 -0700

    feat: enhance LRU prefer clean policy with age tracking and eviction logic

--- src/replacement_policies.c
+++ src/replacement_policies.c
@@ -32,10 +32,10 @@ void lru_cache_access(struct replacement_policy *replacement_policy,
     struct lru_data *lru = (struct lru_data *)replacement_policy->data;
 
     // Find the cache line with the tag
-    int set_start = set_idx * cache_system->associativity;
+    int start_index = set_idx * cache_system->associativity;
     int accessed_index = -1;
     for (uint32_t i = 0; i < lru->associativity; i++) {
-        struct cache_line *line = &cache_system->cache_lines[set_start + i];
+        struct cache_line *line = &cache_system->cache_lines[start_index + i];
         if (line->status != INVALID && line->tag == tag) {
             accessed_index = i;
             break;
@@ -160,23 +160,80 @@ void lru_prefer_clean_cache_access(struct replacement_policy *replacement_policy
                                    struct cache_system *cache_system, uint32_t set_idx,
                                    uint32_t tag)
 {
-    // TODO update the LRU_PREFER_CLEAN replacement policy state given a new
+    // NOTE update the LRU_PREFER_CLEAN replacement policy state given a new
     // memory access
     // NOTE: you may be able to share code with the LRU policy
+    struct lru_data *lru = (struct lru_data *)replacement_policy->data;
+    
+    // Find the cache line with the tag
+    int start_idx = set_idx * cache_system->associativity;
+    int access_index = -1;
+    for (uint32_t i = 0; i < lru->associativity; i++) {
+        struct cache_line *line = &cache_system->cache_lines[start_idx + i];
+        if (line->status != INVALID && line->tag == tag) {
+            access_index = i;
+            break;
+        }
+    }
+    
+    if (access_index == -1) { // Miss
+        return;
+    }
+    
+    // Update the age
+    uint32_t current_age = lru->ages[set_idx][access_index];
+    for (uint32_t i = 0; i < lru->associativity; i++) {
+        if (lru->ages[set_idx][i] > current_age) {
+            lru->ages[set_idx][i]--;
+        }
+    }
+    lru->ages[set_idx][access_index] = lru->associativity - 1;
 }
 
 uint32_t lru_prefer_clean_eviction_index(struct replacement_policy *replacement_policy,
                                          struct cache_system *cache_system, uint32_t set_idx)
 {
     // TODO return the index within the set that should be evicted.
-
-    return 0;
+    struct lru_data *lru_pc = (struct lru_data *)replacement_policy->data;
+    int set_start = set_idx * cache_system->associativity;
+    // First, try to find the least recently used clean line
+    uint32_t oldest_clean_index = UINT32_MAX;
+    uint32_t oldest_clean_age = UINT32_MAX;
+    for (int i = 0; i < cache_system->associativity; i++) {
+        struct cache_line *line = &cache_system->cache_lines[set_start + i];
+        if (line->status == EXCLUSIVE) {  // Clean line
+            if (lru_pc->ages[set_idx][i] < oldest_clean_age) {
+                oldest_clean_age = lru_pc->ages[set_idx][i];
+                oldest_clean_index = i;
+            }
+        }
+    }
+    // If we found a clean line, return its index
+    if (oldest_clean_index != UINT32_MAX) {
+        return oldest_clean_index;
+    }
+    // Otherwise, find the least recently used dirty line
+    uint32_t oldest_dirty_age = UINT32_MAX;
+    uint32_t oldest_dirty_index = 0;
+    for (int i = 0; i < cache_system->associativity; i++) {
+        if (lru_pc->ages[set_idx][i] < oldest_dirty_age) {
+            oldest_dirty_age = lru_pc->ages[set_idx][i];
+            oldest_dirty_index = i;
+        }
+    }
+    return oldest_dirty_index;
 }
 
 void lru_prefer_clean_replacement_policy_cleanup(struct replacement_policy *replacement_policy)
 {
-    // TODO cleanup any additional memory that you allocated in the
+    // NOTE cleanup any additional memory that you allocated in the
     // lru_prefer_clean_replacement_policy_new function.
+    struct lru_data *lru = (struct lru_data *)replacement_policy->data;
+    for (uint32_t i = 0; i < lru->sets; i++) {
+        free(lru->ages[i]);
+    }
+    free(lru->ages);
+    free(lru);
 }
 
 struct replacement_policy *lru_prefer_clean_replacement_policy_new(uint32_t sets,
@@ -187,8 +244,21 @@ struct replacement_policy *lru_prefer_clean_replacement_policy_new(uint32_t sets
     lru_prefer_clean_rp->eviction_index = &lru_prefer_clean_eviction_index;
     lru_prefer_clean_rp->cleanup = &lru_prefer_clean_replacement_policy_cleanup;
 
-    // TODO allocate any additional memory to store metadata here and assign to
+    // NOTE allocate any additional memory to store metadata here and assign to
     // lru_prefer_clean_rp->data.
+    struct lru_data *lru = calloc(1, sizeof(struct lru_data));
+    lru->sets = sets;
+    lru->associativity = associativity;
+    // Allocate the ages 2D array
+    lru->ages = calloc(sets, sizeof(uint32_t *));
+    for (uint32_t i = 0; i< sets; i++) {
+        lru->ages[i] = calloc(associativity, sizeof(uint32_t));
+        // Initialize the ages - initially all lines are 0, 1, 2, ..., associativity-1
+        for (uint32_t j = 0; j < associativity; j++) {
+            lru->ages[i][j] = j;
+        }
+    }
+    lru_prefer_clean_rp->data = lru;
 
     return lru_prefer_clean_rp;
 }
